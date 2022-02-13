fe6e89c9926d87c3a5d04a28cb16975d54823d7a
adindafaayza <99375342+adindafaayza@users.noreply.github.com>
2022-02-13 15:52:27 +0700

    Menambahkan file soal 2-6

---
+++ FloatingPoint.java
@@ -0,0 +1,14 @@
+/**
+ * Program untuk membulatkan suatu nilai
+ * @author Adinda Faayza Malika
+ * @version 1.0
+ * @since 2022-02-12
+ */
+class FloatingPoint {
+    public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        System.out.println(nx);
+    }
+    
+}
