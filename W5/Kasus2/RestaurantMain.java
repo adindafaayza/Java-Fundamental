adddb52110bcee6dcf85bfcc045259c5b7b6815a
adindafaayza <99375342+adindafaayza@users.noreply.github.com>
2022-04-07 01:42:13 +0700

    Add Folder W5
    Kasus2
    Memodifikasi class sehingga memenuhi konsep OOP dan mengembangkan kelas dengan menambahkan method untuk memesan makanan lalu mengurangi stok makanan.
    
    Kasus3
    Dalam studi kasus ini saya belajar alur kerja inheritance dalam java.

---
+++ W5/Kasus2/RestaurantMain.java
@@ -0,0 +1,26 @@
+
+
+public class RestaurantMain {
+    public static void main(String[] args) {
+        Restaurant menu = new Restaurant();
+        menu.tambahMenuMakanan("Bala-Bala", 1_000, 20);
+        Restaurant.nextId();
+        menu.tambahMenuMakanan("Gehu", 1_000, 20);
+        Restaurant.nextId();
+        menu.tambahMenuMakanan("Tahu", 1_000, 0);
+        Restaurant.nextId();
+        menu.tambahMenuMakanan("Molen", 1_000, 20);
+        menu.tampilMenuMakanan();
+
+        menu.pesanMakanan("Molen",2);
+        menu.pesanMakanan("Bala-Bala", 5);
+        menu.pesanMakanan("Tahu", 3);
+        menu.pesanMakanan("Gehu",3);
+        
+        
+        System.out.println("");
+        menu.tampilMenuMakanan();
+        
+    }
+}
+
